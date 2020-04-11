ae0ea0f3d1b2b5563769552cb6db3d00559b9ec4
Sayer Tindall <sayertindall@Sayers-MacBook-Pro.local>
2020-04-11 07:36:23 -0700

    update error message

--- ffmpeg_media_converter.py
+++ ffmpeg_media_converter.py
@@ -3,7 +3,9 @@ from sys import argv
 
 # Name input and output file format
 def main():
+    # File to convert
     file_in = argv[1]
+    # File format & name to convert to
     file_out = argv[2]
 
     # C lib to convert media files
@@ -16,7 +18,7 @@ def main():
 
 if __name__ == '__main__':
     if len(argv) < 3:
-        print("Usage: %s <filename> <input_file> <output_file>",)
+        print("Usage: <filename> <input_file> <output_file>")
     else:
         main()
 
